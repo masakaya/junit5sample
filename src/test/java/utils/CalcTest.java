c915a7284fadd113fe3b8f8b4fb59efe45222444
Masashi Kayahara <sylphs.mb@gmail.com>
2019-08-21 21:58:56 +0900

    Adds Calc#add() test.

--- src/test/java/utils/CalcTest.java
+++ src/test/java/utils/CalcTest.java
@@ -2,15 +2,15 @@ package utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CalcTest {
 
-  private Calc testee;
+  private Calc testee = new Calc();
 
   @Test
   void add() {
-
-
+    int actual = testee.add(5, 6);
+    assertThat(actual).isEqualTo(11);
   }
 }
