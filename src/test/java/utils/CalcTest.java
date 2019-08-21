02381d788055a8cb3b63e4c83d5570c532706cb5
Masashi Kayahara <sylphs.mb@gmail.com>
2019-08-21 21:58:56 +0900

    Reformat code

--- src/test/java/utils/CalcTest.java
+++ src/test/java/utils/CalcTest.java
@@ -1,16 +1,16 @@
 package utils;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CalcTest {
 
-  private Calc testee;
+  private Calc testee = new Calc();
 
   @Test
   void add() {
-
-
+    int actual = testee.add(5, 6);
+    assertThat(actual).isEqualTo(11);
   }
 }
